a4f100b31f756153f37b4463e5c3e06a7c5564d2
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-18 13:07:34 +0530

    random superhero from superheroes library

---
+++ npm/index.js
@@ -0,0 +1,19 @@
+// var generateName = require('sillyname');
+
+//using the "type" : "module"
+// we betterly use import as comparare var
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+// console.log(`My name is ${sillyName}.`)
+
+
+//for marvles
+ 
+import superheroes from "superheroes";
+superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+ 
+superheroes.random();
+//=> 'Spider-Ham'
+console.log(superheroes.random())
