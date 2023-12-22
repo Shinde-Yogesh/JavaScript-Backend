4ad170d5f32bcd81d85743ed2124039c7e29e823
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-22 10:07:45 +0530

    testing the condition using ejs

--- EJS/index.js
+++ EJS/index.js
@@ -3,7 +3,32 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.listen(port, ()=>
-{
-    console.log(`server running on ${port}`)
-})
+app.get("/", (req, res) => {
+  const today = new Date();
+  // new Date("June 20, 2023 11:13:00");
+
+  //Test code
+  // weekend:
+  // new Date("June 24, 2023 11:13:00");
+  // weekday:
+  // new Date("June 20, 2023 11:13:00");
+  const day = today.getDay();
+
+  // console.log(day);
+  let type = "Snoow falls";
+  let adv = "Winner is coming";
+
+  if (day === 0 || day === 6) {
+    type = "the weekend";
+    adv = "it's time to have some fun";
+  }
+
+  res.render("index.ejs", {
+    dayType: type,
+    advice: adv,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
