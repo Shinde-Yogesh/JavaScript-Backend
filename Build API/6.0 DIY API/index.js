28e13e5904273056f78fe7ff6c9c808f99f8cd18
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-24 12:50:03 +0530

    delete and deleteAll and one custom method getAllJokes

--- Build API/6.0 DIY API/index.js
+++ Build API/6.0 DIY API/index.js
@@ -12,6 +12,12 @@ app.get("/random", (req, res) => {
   const randomIndex = Math.floor(Math.random() * jokes.length);
   res.json(jokes[randomIndex]);
 });
+
+//1. GET a the all joke
+app.get("/all_jokes", (req, res) => {
+  res.json(jokes);
+});
+
 //2. GET a specific joke
 app.get("/jokes/:id", (req, res) => {
   const id = parseInt(req.params.id);
@@ -25,15 +31,76 @@ app.get("/filter", (req, res) => {
   res.json(filteredActivities);
 });
 //4. POST a new joke
+app.post("/jokes", (req, res) => {
+  const newJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  jokes.push(newJoke);
+  console.log(jokes.slice(-1));
+  res.json(newJoke);
+});
 
-//5. PUT a joke
+//Put a joke
+app.put("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
 
-//6. PATCH a joke
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
 
-//7. DELETE Specific joke
+  jokes[searchIndex] = replacementJoke;
+  // console.log(jokes);
+  res.json(replacementJoke);
+});
 
-//8. DELETE All jokes
+//Patch a joke
+app.patch("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const existingJoke = jokes.find((joke) => joke.id === id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text || existingJoke.jokeText,
+    jokeType: req.body.type || existingJoke.jokeType,
+  };
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[searchIndex] = replacementJoke;
+  console.log(jokes[searchIndex]);
+  res.json(replacementJoke);
+});
 
+//DELETE Specific joke
+//Optional Edge Case Mangement: Can you think of a situation where we might have an issue deleting
+//a specific joke out of the array? Can you think of a solution?
+app.delete("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  if (searchIndex > -1) {
+    jokes.splice(searchIndex, 1);
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Joke with id: ${id} not found. No jokes were deleted.` });
+  }
+});
+
+//DELETE All jokes
+app.delete("/all", (req, res) => {
+  const userKey = req.query.key;
+  if (userKey === masterKey) {
+    jokes = [];
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `You are not authorised to perform this action.` });
+  }
+});
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
 });
