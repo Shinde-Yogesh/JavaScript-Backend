de7c30ed4584a7449068a2b6edfb192dccdb8a8e
Yogesh Shinde <yogeshshinde2324@gmail.com>
2024-05-31 20:55:11 +0530

    Update solution.js

--- 4.1 EJS Tags/solution.js
+++ 4.1 EJS Tags/solution.js
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// express library install for the API call
+
 app.get("/", (req, res) => {
   const data = {
     title: "EJS Tags",
